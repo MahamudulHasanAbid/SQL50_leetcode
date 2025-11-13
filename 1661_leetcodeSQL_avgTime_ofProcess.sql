bc2fc90bae3e7ee33939ae5027391d6a0ccd157b
Mahamudul Hasan <124416655+MahamudulHasanAbid@users.noreply.github.com>
2025-11-13 16:11:05 +0600

    Create 1661_leetcodeSQL_avgTime_ofProcess.sql

---
+++ 1661_leetcodeSQL_avgTime_ofProcess.sql
@@ -0,0 +1,16 @@
+/* There is a factory website that has several machines each running the same number of processes. Write a solution to find the average time each machine takes to complete a process.
+
+The time to complete a process is the 'end' timestamp minus the 'start' timestamp. The average time is calculated by the total time to complete every process on the machine divided by the number of processes that were run.
+
+The resulting table should have the machine_id along with the average time as processing_time, which should be rounded to 3 decimal places.
+
+Return the result table in any order. */
+
+select a1.machine_id, round(sum(a2.timestamp-a1.timestamp)/count(distinct a1.process_id),3) as processing_time
+from Activity a1
+inner join Activity a2
+on a1.machine_id = a2.machine_id
+and a1.process_id = a2.process_id
+and a1.activity_type = 'start'
+and a2.activity_type = 'end'
+group by a1.machine_id;
