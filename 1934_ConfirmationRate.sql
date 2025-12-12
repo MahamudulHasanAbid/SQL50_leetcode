17a3d7a88faac91d0eb37ce2c47e1d16b09ab8e6
Mahamudul Hasan <124416655+MahamudulHasanAbid@users.noreply.github.com>
2025-12-12 21:33:50 +0600

    Create 1934_ConfirmationRate.sql

---
+++ 1934_ConfirmationRate.sql
@@ -0,0 +1,13 @@
+/*
+The confirmation rate of a user is the number of 'confirmed' messages divided by the total number of requested confirmation messages. The confirmation rate of a user that did not request any confirmation messages is 0. Round the confirmation rate to two decimal places.
+
+Write a solution to find the confirmation rate of each user.
+
+Return the result table in any order.
+*/
+
+select s.user_id, round(sum(if(c.action = 'confirmed',1,0))/count(s.user_id),2) as confirmation_rate
+from signups s
+left join confirmations c
+on s.user_id = c.user_id
+group by s.user_id;
