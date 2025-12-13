aa90ac42b1ae3230c65fb634a40aa8ddf9bf9dc4
Mahamudul Hasan <124416655+MahamudulHasanAbid@users.noreply.github.com>
2025-12-13 18:44:23 +0600

    Create leetcodeSQL620_NotBoringMovie.sql

---
+++ leetcodeSQL620_NotBoringMovie.sql
@@ -0,0 +1,10 @@
+/*  Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+
+Return the result table ordered by rating in descending order.
+
+The result format is in the following example.*/
+select * 
+from Cinema
+where lower(description) not like '%boring%' 
+and id%2 != 0
+order by rating desc;
